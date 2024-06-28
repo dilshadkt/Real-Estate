583fe23da52223283d9279044faaaf8a2ee53f84
Dilshad <hmydilshadkt@gmail.com>
2024-06-28 12:14:03 +0530

    feat : change base url

--- front-end/src/lib/apiRequest.js
+++ front-end/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const apiRequest = axios.create({
   // baseURL: "http://localhost:8080/api/",
-  baseURL: "https://real-estate-jxqx.onrender.com/",
+  baseURL: "https://real-estate-jxqx.onrender.com/api/",
   withCredentials: true,
 });
 
